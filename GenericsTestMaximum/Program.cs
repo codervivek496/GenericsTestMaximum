184aeef3c4ada85a7fc79b8d2cae8a5adb338369
Vivek kumar <viveksinghssm496@gmail.com>
2022-05-25 11:12:25 +0530

    welcome to Test Maximum of 3 Variables

---
+++ GenericsTestMaximum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+    namespace GenericsTestMaximum
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to test maximum of 3 variables");
+        }
+    }
+
+}
