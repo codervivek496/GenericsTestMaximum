b43fafa9afe8201849acb7cec139b27e58554ba9
Vivek kumar <viveksinghssm496@gmail.com>
2022-05-26 16:51:42 +0530

    Maximum among three integers

--- GenericsTestMaximum/Program.cs
+++ GenericsTestMaximum/Program.cs
@@ -1,11 +1,37 @@
 ﻿using System;
-    namespace GenericsTestMaximum
+namespace GenericsTestMaximum
 {
     class program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to test maximum of 3 variables");
+            int choice;
+            do
+            {
+                Console.WriteLine("\n1. Find maximum among three integers");
+
+                Console.WriteLine("0. Exit");
+                Console.Write("Enter your choice : ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        MaxAmongThree maxAmongThree = new MaxAmongThree();
+
+                        //Test case 1:  Max number in First position
+                        Console.WriteLine("Maximum integer is: " + maxAmongThree.MaxAmongThreeInt(3, 2, 1));
+
+                        //Test case 2: Max number in Second position
+                        Console.WriteLine("Maximum integer is: " + maxAmongThree.MaxAmongThreeInt(1, 3, 2));
+
+                        //Test case 3: Max number in Third position
+                        Console.WriteLine("Maximum integer is: " + maxAmongThree.MaxAmongThreeInt(1, 2, 3));
+                        break;
+                    default:
+                        Console.WriteLine("Choose the correct option from the list");
+                        break;
+                }
+            } while (choice != 0);
         }
     }
 
