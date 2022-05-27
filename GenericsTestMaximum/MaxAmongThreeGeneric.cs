9d584aed84f18d072ee38ea3a6deb8e12838b63b
Vivek kumar <viveksinghssm496@gmail.com>
2022-05-27 15:39:02 +0530

    Generics Method

---
+++ GenericsTestMaximum/MaxAmongThreeGeneric.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsTestMaximum
+{
+    internal class MaxAmongThreeGeneric
+    {
+        //Using generic method
+        //Implementing IComparable interface to use CompareTo method
+        public T MaxAmongThree<T>(T val1, T val2, T val3) where T : IComparable
+        {
+            if (val1.CompareTo(val2) > 0 && val1.CompareTo(val3) > 0)
+            {
+                return val1;
+            }
+
+            if (val2.CompareTo(val1) > 0 && val2.CompareTo(val3) > 0)
+            {
+                return val2;
+            }
+
+            if (val3.CompareTo(val1) > 0 && val3.CompareTo(val2) > 0)
+            {
+                return val3;
+            }
+
+            return val1;
+        }
+    }
+}
